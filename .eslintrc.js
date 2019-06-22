805b7b3bf3446ecf688b8f3d617025ff92a30be6
zhangyu <zhangyu@163.com>
2019-06-22 23:49:39 +0800

    修改了代码风格配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,12 @@ module.exports = {
     'plugin:vue/essential',
     '@vue/standard'
   ],
+  // 自定义规则选项配置对象
+  // key:规则代号
+  // value:规则说明
+  // "off" or 0 - 关闭规则，什么都不做
+  // "warn" or 1 - 将规则视为一个警告（不会影响退出码），警告不退出
+  // "error" or 2 - 将规则视为一个错误 (退出码为1)，报错并退出
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
