1fa19430cd66ded74995d2d1b5f80fe290298ae8
zhangyu <zhangyu@163.com>
2019-06-22 23:53:25 +0800

    导入图片资源

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 优先查找文件，如果文件找不到则查找目录
 // 找到目录优先加载目录中的index.js
 
 import router from './router/index'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
