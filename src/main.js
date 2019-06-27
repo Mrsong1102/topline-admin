b8772647befe870be24e0534893c3bcaf93f6558
zhangyu <zhangyu@163.com>
2019-06-27 16:54:41 +0800

    使用axios响应拦截器统一处理401状态码

--- src/main.js
+++ src/main.js
@@ -28,7 +28,12 @@ axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 
 axios.interceptors.request.use(config => {
   const userInfo = JSON.parse(window.localStorage.getItem('user_info'))
-  config.headers.Authorization = `Bearer ${userInfo.token}`
+
+  // 如果登录了，才给那些需要 token 的接口同意添加 token 令牌
+  // 登录相关接口不需要添加 token 令牌
+  if (userInfo) {
+    config.headers.Authorization = `Bearer ${userInfo.token}`
+  }
   return config
 }, function (error) {
   return Promise.reject(error)
@@ -40,10 +45,18 @@ axios.interceptors.request.use(config => {
  */
 
 axios.interceptors.response.use(response => { // >= 200 && < 400 的状态码进入这里
-  console.log('response => ', response)
+  // console.log('response => ', response)
   return response.data.data
 }, error => { // >=400 的状态码进入这里
-  console.log('response error => ', error)
+  const status = error.response.status
+  if (status === 401) {
+    // 务必删除本地存储中的用户信息数据
+    window.localStorage.removeItem('user_info')
+    // 跳转到登录页面
+    router.push({
+      name: 'login'
+    })
+  }
   return Promise.reject(error)
 })
 
