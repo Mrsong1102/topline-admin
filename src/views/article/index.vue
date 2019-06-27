5d744ed6cf779fb9b8cc35ab1dd8ed6249ba0f0e
zhangyu <zhangyu@163.com>
2019-06-27 09:58:57 +0800

    文章列表-快速布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,6 +1,77 @@
 <template>
   <div>
-    <p>Article Component</p>
+    <!-- 筛选区域 -->
+    <el-card class="filter-card">
+      <div slot="header" class="clearfix">
+        <span>筛选条件</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      </div>
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="特殊资源">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="线上品牌商赞助"></el-radio>
+            <el-radio label="线下场地免费"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动形式">
+          <el-date-picker
+          v-model="form.value1"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- /筛选区域 -->
+
+    <!-- 列表 -->
+    <el-card class="list-card">
+      <div slot="header" class="clearfix">
+        <span>共找到15条符合条件的内容</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      </div>
+        <!-- table表格 -->
+        <el-table
+        :data="tableData"
+        style="width: 100%">
+          <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+          </el-table-column>
+          <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+          </el-table-column>
+          <el-table-column
+          prop="address"
+          label="地址">
+          </el-table-column>
+        </el-table>
+        <!-- /table表格 -->
+
+        <!-- 分页 -->
+        <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000">
+        </el-pagination>
+        <!-- /分页 -->
+    </el-card>
+    <!-- /列表 -->
+
   </div>
 </template>
 
@@ -9,12 +80,50 @@ export default {
   name: 'ArticleList',
   data () {
     return {
-
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: '',
+        value1: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.filter-card {
+  margin-bottom: 20px;
+}
+.el-pagination {
+  text-align: center;
+  padding-top: 20px;
+}
 </style>
