6f05c72a052f9b960db6861a1e24546dbe6f8af1
zhangyu <zhangyu@163.com>
2019-06-30 16:52:55 +0800

    评论管理-修改文章评论状态-处理开关禁用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -10,6 +10,7 @@
       <el-table-column label="允许评论">
         <template slot-scope="scope">
           <el-switch
+            :disabled="scope.row.changeLoading"
             v-model="scope.row.comment_status"
             active-color="#13ce66"
             inactive-color="#ff4949"
@@ -41,11 +42,15 @@ export default {
           response_type: 'comment'
         }
       }).then(data => {
+        data.results.forEach(item => {
+          item.changeLoading = false
+        })
         this.articles = data.results
       })
     },
 
     handleChangeCommentStatus (item) {
+      item.changeLoading = true // 禁用开关的点击状态
       this.$http({
         method: 'PUT',
         url: '/comments/status',
@@ -53,15 +58,18 @@ export default {
           article_id: item.id.toString()
         },
         data: {
-            allow_comment: item.comment_status
+          allow_comment: item.comment_status
         }
       }).then(() => {
         this.$message({
           type: 'success',
           message: `${item.comment_status ? '启用' : '关闭'}评论状态成功`
         })
+        item.changeLoading = false // 启用开关的点击状态
       }).catch(err => {
         console.log(err)
+        item.changeLoading = false // 启用开关的点击状态
+        item.comment_status = !item.comment_status
         this.$message.error('修改评论状态失败！')
       })
     }
