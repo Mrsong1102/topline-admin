3422c302f7e24474ad49b9dd04df503907680be2
zhangyu <zhangyu@163.com>
2019-07-01 14:58:09 +0800

    账户设置-用户头像配置使用upload上传组件-头像上传完成

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -26,7 +26,37 @@
           </el-form-item>
         </el-form>
       </el-col>
-      <el-col :offset="2" :span="4"></el-col>
+      <el-col :offset="2" :span="4">
+        <!--
+          el-upload 上传组件，他会自动将用户选择的图片去请求上传，我们需要做的就是配置一下
+            action 请求地址
+            由于他用的自己内部的请求，不是使用的axios 去发请求
+              完整路径
+              它的请求也不会经过 axios 拦截器，所以需要手动配置 token
+              不支持自定义请求方法
+         -->
+        <!-- <el-upload
+          class="avatar-uploader"
+          action="http://ttapi.research.itcast.cn/mp/v1_0/user/photo"
+          :headers="{ Authorization: token }"
+          name="photo"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload">
+          <img class="avatar" v-if="userInfo.photo" :src="userInfo.photo">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload> -->
+        <el-upload
+          class="avatar-uploader"
+          action="http://ttapi.research.itcast.cn/mp/v1_0/user/photo"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          :http-request="handleUpdate">
+          <img class="avatar" v-if="userInfo.photo" :src="userInfo.photo">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-col>
     </el-row>
   </el-card>
 </template>
@@ -36,7 +66,8 @@ export default {
   name: 'AccountSetting',
   data () {
     return {
-      userInfo: {}
+      userInfo: {},
+      token: `Bearer ${JSON.parse(window.localStorage.getItem('user_info')).token}`
     }
   },
   created () {
@@ -54,7 +85,7 @@ export default {
     },
 
     handleUpdate () {
-      const {name, intro, email } = this.userInfo
+      const { name, intro, email } = this.userInfo
       this.$http({
         method: 'PATCH',
         url: '/user/profile',
@@ -72,11 +103,61 @@ export default {
         console.log(err)
         this.$message.error('更新用户信息失败！')
       })
+    },
+
+    handleAvatarSuccess () {},
+    beforeAvatarUpload () {},
+    handleUpload (UploadConfig) {
+      // axios 上传文件
+      // 1.构建一个 FormData 表单对象
+      // 将文件对象添加到 FormData 中
+      // 2.将 FormData 配置到请求体 data 选项中
+      const formData = new FormData()
+      formData.append('photo', UploadConfig.file)
+      this.$http({
+        method: 'PATCH',
+        url: '/user/photo',
+        data: formData
+        // data: {
+        //   photo: 文件对象
+        // }
+      }).then(data => {
+        this.userInfo.photo = data.photo
+        this.$message({
+          type: 'success',
+          message: '上传头像成功'
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('上传头像失败！')
+      })
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
