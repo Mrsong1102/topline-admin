ac5d88937ee6c48613189fe97fa40f0c76452dc0
zhangyu <zhangyu@163.com>
2019-07-02 00:41:48 +0800

    处理添加和编辑触发表单变脏的问题

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -65,6 +65,13 @@ import 'quill/dist/quill.bubble.css'
 
 import { quillEditor } from 'vue-quill-editor'
 
+/**
+ * 如果是更新，则在第一次更新数据之后开启监视
+ * 如果是添加，则一上来就开启监视
+ * 如果是从更新页面导航到发布页面，则清空表单数据
+ * 如果是从发布页面导航发布页面，则重新加载编辑页面的数据
+ */
+
 export default {
   name: 'AppPublish',
 
@@ -86,7 +93,8 @@ export default {
       editorOption: {}, // 富文本编辑器相关参数选项
       editLoading: false,
       publishLoading: false,
-      formDirty: false
+      formDirty: false,
+      firstEditData: false
     }
   },
 
@@ -96,15 +104,20 @@ export default {
    * 注意：这里配置的监视无法取消，会重复监视
    *      如果需要有一个可以取消的监视，则需要通过 this.$watch 的方式进行监视
    */
-  watch: {
-    articleForm: {
-      handler () { // 当被监视数据发生改变的时候会被调用
-        this.formDirty = true
-      },
-      deep: true // 对象、数组类型需要配置深度监视，如果是普通数据不需要
-      // immediate: false // 默认只有当被监视成员发生改变的时候才会调用监视函数，如果希望初始的时候就调用一次，则可以配置该值为 true
-    }
-  },
+  // watch: {
+  //   articleForm: {
+  //     handler () { // 当被监视数据发生改变的时候会被调用
+  //       // 如果是第一次编辑更新导致数据的改变，不要让 this.formDirty 为 true
+  //       if (this.firstEditData) {
+  //         this.formDirty = false
+  //       } else { // 否则都是用户操作表单导致的数据修改
+  //         this.formDirty = true
+  //       }
+  //     },
+  //     deep: true // 对象、数组类型需要配置深度监视，如果是普通数据不需要
+  //     // immediate: false // 默认只有当被监视成员发生改变的时候才会调用监视函数，如果希望初始的时候就调用一次，则可以配置该值为 true
+  //   }
+  // },
 
   computed: {
     editor () {
@@ -120,6 +133,12 @@ export default {
 
   created () {
     this.isEdit && this.loadArticle()
+
+    // 如果是发布页面，则直接开启监视
+    if (this.$route.name === 'publish') {
+      // 开启监视
+      this.watchForm()
+    }
   },
 
   mounted () {
@@ -133,8 +152,26 @@ export default {
         method: 'GET',
         url: `/articles/${this.articleId}`
       }).then(data => {
+        // 编辑页面,修改表单数据,动态加载表单内容
+        // 这个数据的修改也会触发对表单数据的监视
+        // 这个数据非用户修改的数据,所以不要让其变脏
         this.articleForm = data
+
         this.editLoading = false
+
+        // 如果你此时其操作上面数据驱动视图的更新，此时拿到的 DOM 是更新之前的
+        // 如果你需要在数据驱动改变影响视图更新之后做一些 DOM 操作，可以把代码写到 this.$nextForm()
+
+        // 数据的修改并不是立即出发视图的更新的
+        // 这个监视器的监视咸鱼正正的数据修改执行
+        // 这种代码的执行，即便上面先修改数据，后面去监视也会监视到
+        // this.watchForm()
+
+        // Vue 提供了这样的一个 API，简单理解就是延时调用
+        this.$nextTick(() => {
+          // 更新数据加载好以后，开启监视
+          this.watchForm()
+        })
       }).catch(err => {
         console.log(err)
         this.$message.error('加载文章详情失败')
@@ -191,6 +228,17 @@ export default {
       }).catch(err => {
         console.log(err)
         this.$message.error('发表失败')
+      })
+    },
+
+    watchForm () {
+      const unWatch = this.$watch('articleForm', function () {
+        console.log('watchForm')
+        this.formDirty = true
+        // 关闭监视器
+        unWatch()
+      }, {
+        deep: true
       })
     }
   },
