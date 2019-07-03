78d3cb5b69206a753a073691c6a8189bb2d16363
zhangyu <zhangyu@163.com>
2019-07-03 22:25:52 +0800

    封面处理-显示对话框

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="box-wrap">
-    <p>点击选择图片</p>
-    <i
-      style="margin-top: 20px;
-      font-size: 90px; color: #eee;"
-      class="iconfont icon-image_upload"
-    ></i>
-    <img width="120" src="http://toutiao.meiduo.site/FvGUrO7Ru_T2ToQwn_OznfwKnjoL">
+  <div>
+    <div class="box-wrap" @click="handleShowMediaBox">
+      <p>点击选择图片</p>
+      <i
+        style="margin-top: 20px;
+        font-size: 90px; color: #eee;"
+        class="iconfont icon-image_upload"
+      ></i>
+      <img width="120" src="http://toutiao.meiduo.site/FvGUrO7Ru_T2ToQwn_OznfwKnjoL">
+    </div>
+
+    <!-- 对话框 -->
+    <el-dialog
+      title="素材"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- /对话框 -->
   </div>
 </template>
 
@@ -15,6 +30,16 @@ export default {
   name: 'UploadImage',
   data () {
     return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleShowMediaBox () {
+      // 1.显示弹框
+      this.dialogVisible = true
+      // 2.在弹框中展示图片素材
+      // 3.用户可以点击切换图片的选中状态
+      // 4.用户点击确定，我们拿到所选的图片链接，将数据给谁？
     }
   }
 }
